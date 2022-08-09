eac4188160f75e482048de5046e4d5b9410f333a
darianchen <darianychen@gmail.com>
2022-08-09 15:13:07 -0400

    create board class

---
+++ class Board.rb
@@ -0,0 +1,19 @@
+class Board
+
+    def initialize
+        @rows = Array.new(8) {Array.new(8)}
+    end
+
+    def []
+
+    end
+
+    def []=()
+
+    end
+
+    def move_piece(start_pos,end_pos)
+    
+    end
+end
+
